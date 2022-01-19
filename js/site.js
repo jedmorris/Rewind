5c0715ebd993f768d0c99b2399dbd9c27652a00d
Jed Morris <jemorris110@gmail.com>
2022-01-19 09:32:36 -0800

    built program outline structure in site.js

--- js/site.js
+++ js/site.js
@@ -1,13 +1,25 @@
 
 // pull user entered string from page
 function getValues() {
+	let userString = document.getElementById("revString").value.length
 
+	// call reverseString
+	let revString = reverseString(userString);
 
-
+	// call displayData
+	displayData(revString);
 
 	Swal.fire(
 		'Button Clicked!',
 		userMessage,
 		'success!'
 	)
+}
+
+function reverseString(userString) {
+
+}
+
+function displayData(revString) {
+
 }
